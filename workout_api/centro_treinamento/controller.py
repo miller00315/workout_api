ea5a2a929d9d233a8f7a9068ac1087cf26c6e149
Miller <miller00314@gmail.com>
2024-06-12 13:17:26 -0300

    ajustando a data

--- workout_api/centro_treinamento/controller.py
+++ workout_api/centro_treinamento/controller.py
@@ -1,4 +1,36 @@
-from fastapi import APIRouter
+from datetime import datetime
+from uuid import uuid4
+from fastapi import APIRouter, Body, HTTPException, status
 
+from workout_api.centro_treinamento.models import CentroTreinamentoModel
+from workout_api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut
+from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
+
+@router.post(
+    '/',
+    summary='Criar um novo centro de treinamento',
+    status_code=status.HTTP_201_CREATED,
+    response_model=CentroTreinamentoOut,
+)
+async def post(
+    db_session: DatabaseDependency,
+    centro_treinamento_in: CentroTreinamentoIn = Body(...)
+):
+    centro_treinamento_out = CentroTreinamentoOut(
+        id=uuid4(), 
+        created_at=datetime.now(), 
+        **centro_treinamento_in.model_dump()
+    )
+
+    centro_treinamento_model = CentroTreinamentoModel(
+        **centro_treinamento_out.model_dump(),
+    )
+
+    db_session.add(centro_treinamento_model)
+
+    await db_session.commit()
+
+    return centro_treinamento_out
+
--- workout_api/routers.py
+++ workout_api/routers.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from workout_api.atleta.controller import router as atleta
 from workout_api.categorias.controller import router as categoria
+from workout_api.centro_treinamento.controller import router as centro_treinamento
 
 api_router = APIRouter()
 
 api_router.include_router(atleta, prefix='/atletas', tags=['atletas'])
 api_router.include_router(categoria, prefix='/categoria', tags=['categorias'])
+api_router.include_router(centro_treinamento, prefix='/centro_treimnamento', tags=['centro treinamento'])
